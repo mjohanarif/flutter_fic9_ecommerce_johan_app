2fc7fbe0af289e7f2692d461477d3db788a003ca
mjohanarif <m.johanarifin@gmail.com>
2023-11-01 22:33:52 +0700

    chore clear warning code

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -66,192 +66,190 @@ class _HomePageState extends State<HomePage> {
     ];
 
     return Scaffold(
-      body: SingleChildScrollView(
+      body: ListView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const SpaceHeight(20.0),
-            Row(
-              children: [
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Alamat Pengiriman",
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w400,
-                        color: ColorName.grey,
-                      ),
+        children: [
+          const SpaceHeight(20.0),
+          Row(
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Alamat Pengiriman",
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400,
+                      color: ColorName.grey,
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          "Sleman, DI Yogyakarta",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
-                            color: ColorName.primary,
-                          ),
-                        ),
-                        SpaceWidth(5.0),
-                        Icon(
-                          Icons.expand_more,
-                          size: 18.0,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Sleman, DI Yogyakarta",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
                           color: ColorName.primary,
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                Row(
-                  children: [
-                    badges.Badge(
-                      badgeContent: BlocBuilder<CartBloc, CartState>(
-                        builder: (context, state) {
-                          return state.maybeWhen(
-                            orElse: () {
-                              return Text(
-                                '0',
-                                style: TextStyle(color: Colors.white),
-                              );
-                            },
-                            loaded: (carts) {
-                              int quantity = 0;
-                              for (var cart in carts) {
-                                quantity += cart.quantity;
-                              }
-                              return Text(
-                                quantity.toString(),
-                                style: TextStyle(color: Colors.white),
-                              );
-                            },
-                          );
-                        },
                       ),
-                      badgeStyle: badges.BadgeStyle(
-                        badgeColor: Colors.red,
+                      SpaceWidth(5.0),
+                      Icon(
+                        Icons.expand_more,
+                        size: 18.0,
+                        color: ColorName.primary,
                       ),
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const CartPage(),
-                            ),
-                          );
-                        },
-                        icon: Image.asset(
-                          Images.iconBuy,
-                          height: 24.0,
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SizedBox()),
-                          );
-                        },
-                        icon: Image.asset(
-                          Images.iconNotificationHome,
-                          height: 24.0,
-                        )),
-                  ],
-                ),
-              ],
-            ),
-            const SpaceHeight(16.0),
-            SearchInput(
-              controller: searchController,
-              onChanged: (value) {},
-            ),
-            const SpaceHeight(16.0),
-            ImageSlider(items: images),
-            const SpaceHeight(12.0),
-            const Text(
-              "Kategori",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
-                color: ColorName.primary,
+                    ],
+                  ),
+                ],
               ),
-            ),
-            const SpaceHeight(12.0),
-            Row(
-              children: [
-                Flexible(
-                  child: CategoryButton(
-                    imagePath: Images.fashion1,
-                    label: 'Pakaian',
-                    onPressed: () {},
+              const Spacer(),
+              Row(
+                children: [
+                  badges.Badge(
+                    badgeContent: BlocBuilder<CartBloc, CartState>(
+                      builder: (context, state) {
+                        return state.maybeWhen(
+                          orElse: () {
+                            return const Text(
+                              '0',
+                              style: TextStyle(color: Colors.white),
+                            );
+                          },
+                          loaded: (carts) {
+                            int quantity = 0;
+                            for (var cart in carts) {
+                              quantity += cart.quantity;
+                            }
+                            return Text(
+                              quantity.toString(),
+                              style: const TextStyle(color: Colors.white),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    badgeStyle: const badges.BadgeStyle(
+                      badgeColor: Colors.red,
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const CartPage(),
+                          ),
+                        );
+                      },
+                      icon: Image.asset(
+                        Images.iconBuy,
+                        height: 24.0,
+                      ),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: CategoryButton(
-                    imagePath: Images.fashion2,
-                    label: 'Pakaian',
-                    onPressed: () {},
-                  ),
-                ),
-                Flexible(
-                  child: CategoryButton(
-                    imagePath: Images.fashion3,
-                    label: 'Pakaian',
-                    onPressed: () {},
-                  ),
-                ),
-                Flexible(
-                  child: CategoryButton(
-                    imagePath: Images.more,
-                    label: 'Pakaian',
-                    onPressed: () {},
-                  ),
-                ),
-              ],
-            ),
-            const SpaceHeight(16.0),
-            const Text(
-              "Produk",
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
-                color: ColorName.primary,
+                  IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SizedBox()),
+                        );
+                      },
+                      icon: Image.asset(
+                        Images.iconNotificationHome,
+                        height: 24.0,
+                      )),
+                ],
               ),
+            ],
+          ),
+          const SpaceHeight(16.0),
+          SearchInput(
+            controller: searchController,
+            onChanged: (value) {},
+          ),
+          const SpaceHeight(16.0),
+          ImageSlider(items: images),
+          const SpaceHeight(12.0),
+          const Text(
+            "Kategori",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+              color: ColorName.primary,
             ),
-            const SpaceHeight(8.0),
-            BlocBuilder<ProductsBloc, ProductsState>(
-              builder: (context, state) {
-                return state.maybeWhen(
-                  orElse: () {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                  loaded: (data) {
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 10.0,
-                        mainAxisSpacing: 55.0,
-                      ),
-                      itemCount: data.data.length,
-                      itemBuilder: (context, index) => ProductCard(
-                        data: data.data[index],
-                      ),
-                    );
-                  },
-                );
-              },
+          ),
+          const SpaceHeight(12.0),
+          Row(
+            children: [
+              Flexible(
+                child: CategoryButton(
+                  imagePath: Images.fashion1,
+                  label: 'Pakaian',
+                  onPressed: () {},
+                ),
+              ),
+              Flexible(
+                child: CategoryButton(
+                  imagePath: Images.fashion2,
+                  label: 'Pakaian',
+                  onPressed: () {},
+                ),
+              ),
+              Flexible(
+                child: CategoryButton(
+                  imagePath: Images.fashion3,
+                  label: 'Pakaian',
+                  onPressed: () {},
+                ),
+              ),
+              Flexible(
+                child: CategoryButton(
+                  imagePath: Images.more,
+                  label: 'Pakaian',
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+          const SpaceHeight(16.0),
+          const Text(
+            "Produk",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+              color: ColorName.primary,
             ),
-          ],
-        ),
+          ),
+          const SpaceHeight(8.0),
+          BlocBuilder<ProductsBloc, ProductsState>(
+            builder: (context, state) {
+              return state.maybeWhen(
+                orElse: () {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+                loaded: (data) {
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 55.0,
+                    ),
+                    itemCount: data.data.length,
+                    itemBuilder: (context, index) => ProductCard(
+                      data: data.data[index],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
