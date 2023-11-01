ce21badf466c8b8eef52a15a6cc96ca8a27b29d4
mjohanarif <m.johanarifin@gmail.com>
2023-11-01 17:05:10 +0700

    fix clean warning code

--- lib/presentation/account/account_page.dart
+++ lib/presentation/account/account_page.dart
@@ -16,13 +16,14 @@ class _AccountPageState extends State<AccountPage> {
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
-            await AuthLocalDataSource().removeAuthData();
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const LoginPage(),
-              ),
-            );
+            await AuthLocalDataSource().removeAuthData().then(
+                  (value) => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
+                    ),
+                  ),
+                );
           },
           child: const Text(
             'Log out',
--- lib/presentation/home/bloc/products/products_state.dart
+++ lib/presentation/home/bloc/products/products_state.dart
@@ -3,7 +3,7 @@ part of 'products_bloc.dart';
 @freezed
 class ProductsState with _$ProductsState {
   const factory ProductsState.initial() = _Initial;
-  const factory ProductsState.Loading() = _Loading;
-  const factory ProductsState.Loaded(ProductResponseModel data) = _Loaded;
-  const factory ProductsState.Error(String message) = _Error;
+  const factory ProductsState.loading() = _Loading;
+  const factory ProductsState.loaded(ProductResponseModel data) = _Loaded;
+  const factory ProductsState.error(String message) = _Error;
 }
