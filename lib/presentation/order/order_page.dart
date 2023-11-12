e985a7feb5a3da53f97bcdaa1521c67228b3f9a3
mjohanarif <m.johanarifin@gmail.com>
2023-11-12 20:27:25 +0700

    feat add tabbar for status orders

--- lib/presentation/order/order_page.dart
+++ lib/presentation/order/order_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_fic9_ecommerce_johan_app/common/components/spaces.dart';
 import 'package:flutter_fic9_ecommerce_johan_app/presentation/order/bloc/buyer_order/buyer_order_bloc.dart';
-import 'package:flutter_fic9_ecommerce_johan_app/presentation/order/models/transaction_model.dart';
 import 'package:flutter_fic9_ecommerce_johan_app/presentation/order/widgets/order_card.dart';
 
 class OrderPage extends StatefulWidget {
@@ -13,19 +12,12 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
-  final List<TransactionModel> transactions = [
-    TransactionModel(
-      noResi: 'QQNSU346JK',
-      status: 'Dikirim',
-      quantity: 2,
-      price: 1900000,
-    ),
-    TransactionModel(
-      noResi: 'SDG1345KJD',
-      status: 'Diterima',
-      quantity: 2,
-      price: 900000,
-    ),
+  List<String> status = [
+    "waiting-payment",
+    "packaging",
+    "on-delivery",
+    "done",
+    "cancel"
   ];
 
   @override
@@ -38,38 +30,69 @@ class _OrderPageState extends State<OrderPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pesanan'),
-      ),
-      body: BlocBuilder<BuyerOrderBloc, BuyerOrderState>(
-        builder: (context, state) {
-          return state.maybeWhen(
-            orElse: () {
-              return const SizedBox();
-            },
-            loading: () => const Center(
-              child: CircularProgressIndicator(),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: status.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pesanan'),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(20),
+            child: TabBar(
+              tabs: status
+                  .map((e) => Text(
+                        e,
+                        style: const TextStyle(fontSize: 16),
+                      ))
+                  .toList(),
+              isScrollable: true,
+              labelPadding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
             ),
-            error: (message) => Center(
-              child: Text(message),
-            ),
-            loaded: (response) {
-              if (response.isEmpty) {
-                return const Center(
-                  child: Text('Tidak ada data'),
-                );
-              }
-              return ListView.separated(
-                itemBuilder: (context, index) => OrderCard(
-                  data: response[index],
-                ),
-                separatorBuilder: (context, index) => const SpaceHeight(16),
-                itemCount: response.length,
-              );
-            },
-          );
-        },
+          ),
+        ),
+        body: TabBarView(
+            children: status
+                .map(
+                  (e) => BlocBuilder<BuyerOrderBloc, BuyerOrderState>(
+                    builder: (context, state) {
+                      return state.maybeWhen(
+                        orElse: () {
+                          return const SizedBox();
+                        },
+                        loading: () => const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        error: (message) => Center(
+                          child: Text(message),
+                        ),
+                        loaded: (response) {
+                          final data = response
+                              .where(
+                                  (element) => element.attributes.status == e)
+                              .toList();
+                          if (response.isEmpty || data.isEmpty) {
+                            return const Center(
+                              child: Text('Tidak ada data'),
+                            );
+                          }
+
+                          return ListView.separated(
+                            padding: const EdgeInsets.all(16),
+                            itemBuilder: (context, index) => OrderCard(
+                              data: data[index],
+                            ),
+                            separatorBuilder: (context, index) =>
+                                const SpaceHeight(16),
+                            itemCount: data.length,
+                          );
+                        },
+                      );
+                    },
+                  ),
+                )
+                .toList()),
       ),
     );
   }
